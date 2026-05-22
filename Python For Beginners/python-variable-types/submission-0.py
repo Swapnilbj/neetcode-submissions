0d7761ab2c8955322daf250fcbe6800ea8adaa05
Swapnilbj <101309004+Swapnilbj@users.noreply.github.com>
2026-05-22 09:07:15 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type=10
+float_type=99.99
+bool_type=True
+string_type="Test"
+list_type=["1","2","3"]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
